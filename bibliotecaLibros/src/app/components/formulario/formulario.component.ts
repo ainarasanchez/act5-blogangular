5a2f51cc245981f77b07d62655647a5d5b8cf391
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-02-26 21:16:55 +0100

    Inicializamos el componente

--- bibliotecaLibros/src/app/components/formulario/formulario.component.ts
+++ bibliotecaLibros/src/app/components/formulario/formulario.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ILibro } from '../../interfaces/ilibro.interface';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  nuevoLibro!: ILibro;
 
 }
