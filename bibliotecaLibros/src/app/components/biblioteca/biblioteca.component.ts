72ff0aa6acfdeabb7cac58c6a0731e3ab946d340
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-02-27 21:43:44 +0100

    Creamos @Input y libro
    Utilizamos ngOnChanges con un ForEach + la estructura que queramos que tenga el listado

--- bibliotecaLibros/src/app/components/biblioteca/biblioteca.component.ts
+++ bibliotecaLibros/src/app/components/biblioteca/biblioteca.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ILibro } from '../../interfaces/ilibro.interface';
 
 @Component({
   selector: 'app-biblioteca',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './biblioteca.component.css'
 })
 export class BibliotecaComponent {
+  @Input() libros: ILibro[] = [];
+  libro: string = ""
+
+  ngOnChanges() {
+    this.libro = ""
+    this.libros.forEach(libro => {
+      this.libro += `<figure class="portada"> 
+        <img src="${libro.url}" alt="${libro.titulo}"> 
+        <h3>${libro.titulo}</h3>
+        <p>Autor: <strong>${libro.autor}</strong></p>
+        <p>Descripción: ${libro.descripcion}</p>
+        <p>Fecha de publicación: <em>${libro.fechaPublicacion}</em></p>
+      </figure>`
+    })
+  }
 
 }
